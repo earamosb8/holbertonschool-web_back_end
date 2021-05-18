875a7222c580e55e4cbbca8dda55119a91b46ea5
earamosb8 <earamosb8@gmail.com>
2021-05-18 15:29:14 -0500

    add task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """function to_str that takes a float n
+       as argument and returns the string
+       representation of the float."""
+    return str(n)

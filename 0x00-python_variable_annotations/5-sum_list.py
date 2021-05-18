f69e21cda39859f821de4d0e9885b61eab4f8e09
earamosb8 <earamosb8@gmail.com>
2021-05-18 15:43:16 -0500

    add task 4 and 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module - list of floats
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """function sum_list which takes a list
+       input_list of floats as argument
+       and returns their sum as a float."""
+    return sum(input_list)

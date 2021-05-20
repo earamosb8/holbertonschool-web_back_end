2715e629a49d68302f2436b5fd888cdb1acfe8b9
earamosb8 <earamosb8@gmail.com>
2021-05-20 10:24:11 -0500

    add task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Let's execute multiple coroutines at the same time with async
+"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n should return the list of all the delays (float values).
+       The list of the delays should be in ascending order without using
+       sort() because of concurrency."""
+    numbers = [await wait_random(max_delay) for _ in range(n)]
+    return sorted(numbers)

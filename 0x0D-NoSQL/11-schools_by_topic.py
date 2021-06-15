a0192047ef2ceb50e518f7ed29e0b1590178e807
earamosb8 <earamosb8@gmail.com>
2021-06-15 16:32:58 -0500

    add task 11

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 11. Where can I learn Python? """
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic: str):
+    """ function that returns the list of
+	school having a specific topic
+    """
+    query: dict = {"topics": topic}
+    schools: list = []
+
+    for school in mongo_collection.find(query):
+        schools.append(school)
+
+    return schools

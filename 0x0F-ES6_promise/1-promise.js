2b15d84d39407a1a3f2139386118e5c98e36b504
Eduardo Ramos <60366913+earamosb8@users.noreply.github.com>
2021-06-22 23:54:22 -0500

    Update 1-promise.js

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,16 +1,9 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === 'true') {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
     } else {
-      reject(
-        new Error('The fake API is not working currently'),
-      );
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
-
-export default getFullResponseFromAPI;

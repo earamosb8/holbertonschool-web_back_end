c71843bc88488d9d1b37a8b3e0bb4b3186a08178
earamosb8 <earamosb8@gmail.com>
2021-07-05 22:49:17 -0500

    fix tas k1

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-// Gets input from user and writes it to stdout
-process.stdin.setEncoding('utf-8');
-
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.resume();
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) process.stdout.write(`Your name is: ${name}\n`);
-});
-// if stdout process in stream comes from the terminal, end process
-if (process.stdout.isTTY) {
-  process.stdin.on('end', () => {
+  process.stdout.write(`Your name is: ${name}`);
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
     process.stdout.write('This important software is now closing\n');
-  });
-}
+    process.exit();
+  }
+});
